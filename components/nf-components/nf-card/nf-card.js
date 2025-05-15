4f3e5402651c7cdf755cf19cc167665750381536
“FishNoNoNo” <867663065@qq.com>
2025-05-15 19:40:40 +0800

    .

---
+++ components/nf-components/nf-card/nf-card.js
@@ -0,0 +1,27 @@
+// components/nf-components/nf-card/nf-card.js
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    width:{
+      type:String,
+      value:'100vw'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
